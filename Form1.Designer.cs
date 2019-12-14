013f3980710e956f9205929590f2ffe6ea0af177
PingX <48038690+PingXD@users.noreply.github.com>
2019-12-14 18:14:59 +0800

    第一次提交

---
+++ Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace DKP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.read = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GameID = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.addDKPnum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textRemark = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.DKPnumlabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.caxunID = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // read
+            // 
+            this.read.Location = new System.Drawing.Point(140, 441);
+            this.read.Name = "read";
+            this.read.Size = new System.Drawing.Size(75, 23);
+            this.read.TabIndex = 3;
+            this.read.Text = "读取";
+            this.read.UseVisualStyleBackColor = true;
+            this.read.Click += new System.EventHandler(this.read_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "增加",
+            "减少"});
+            this.comboBox1.Location = new System.Drawing.Point(69, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 20);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.GameID);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Location = new System.Drawing.Point(140, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(193, 111);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "添加成员";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "添加角色：";
+            // 
+            // GameID
+            // 
+            this.GameID.Location = new System.Drawing.Point(74, 30);
+            this.GameID.Name = "GameID";
+            this.GameID.Size = new System.Drawing.Size(100, 21);
+            this.GameID.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(58, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "添加";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.addDKPnum);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textRemark);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Location = new System.Drawing.Point(140, 129);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(193, 165);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "修改DKP";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "数值:";
+            // 
+            // addDKPnum
+            // 
+            this.addDKPnum.Location = new System.Drawing.Point(69, 71);
+            this.addDKPnum.Name = "addDKPnum";
+            this.addDKPnum.Size = new System.Drawing.Size(100, 21);
+            this.addDKPnum.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "修改理由";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "选项";
+            // 
+            // textRemark
+            // 
+            this.textRemark.Location = new System.Drawing.Point(69, 106);
+            this.textRemark.Name = "textRemark";
+            this.textRemark.Size = new System.Drawing.Size(100, 21);
+            this.textRemark.TabIndex = 5;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(58, 136);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "确认";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.DKPnumlabel);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.caxunID);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Location = new System.Drawing.Point(140, 300);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(193, 130);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "查询DKP";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // DKPnumlabel
+            // 
+            this.DKPnumlabel.AutoSize = true;
+            this.DKPnumlabel.Location = new System.Drawing.Point(98, 74);
+            this.DKPnumlabel.Name = "DKPnumlabel";
+            this.DKPnumlabel.Size = new System.Drawing.Size(0, 12);
+            this.DKPnumlabel.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 12);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "该角色的DKP为:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "角色名";
+            // 
+            // caxunID
+            // 
+            this.caxunID.Location = new System.Drawing.Point(58, 35);
+            this.caxunID.Name = "caxunID";
+            this.caxunID.Size = new System.Drawing.Size(100, 21);
+            this.caxunID.TabIndex = 1;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(58, 101);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "查询";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(122, 452);
+            this.checkedListBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 475);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.read);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "无间门DKP管理器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button read;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox GameID;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textRemark;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label DKPnumlabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox caxunID;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox addDKPnum;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace DKP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button4_Click(object sender, EventArgs e)//添加角色
+        {   
+            string NameID = GameID.Text;
+            string constr = string.Format("Database=Exam;Data Source = 69.194.11.204; User Id = root; Password=Abc123.@;CharSet=utf8;port=3306;Allow User Variables=True");
+            MySqlConnection conn = new MySqlConnection(constr);
+            string sqlcom = string.Format("INSERT INTO DKP (Name,DKPTotal) VALUES ('{0}','0')", NameID);
+            //MySqlDataAdapter sda = new MySqlDataAdapter(sqlcom, conn);
+            conn.Open();
+            MySqlCommand sqlco = new MySqlCommand(sqlcom, conn);
+            
+            int r = sqlco.ExecuteNonQuery();
+            if (r == 1)
+            {
+                MessageBox.Show("添加成功");
+                checkedListBox1.Items.Clear();
+                
+                
+                string sqlcom1 = string.Format("select Name from DKP");
+                MySqlDataAdapter sda = new MySqlDataAdapter(sqlcom1, conn);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                //checkedListBox1 = dt;
+                //checkedListBox1.DisplayMember = "Name";
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    this.checkedListBox1.Items.Add(dt.Rows[i][0], false);//设置不选定                            
+                }
+            }
+            else
+            {
+                MessageBox.Show("!!!!!!!!!添加失败!!!!!!!!! ");
+            }
+            conn.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)//查询角色DKP
+        {
+            if (caxunID.Text == "")
+            {
+                MessageBox.Show("请输入角色ID");
+            }
+            else
+            {
+                string NameIDcaxun = caxunID.Text;
+                string constr = string.Format("Database=Exam;Data Source = 69.194.11.204; User Id = root; Password=Abc123.@;CharSet=utf8;port=3306;Allow User Variables=True");
+                MySqlConnection conn = new MySqlConnection(constr);
+                string sqlcom = string.Format("select * from DKP where Name='{0}' ", NameIDcaxun);
+                MySqlDataAdapter sda = new MySqlDataAdapter(sqlcom, conn);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                string DKPnumbercaxun = dt.Rows[0][7].ToString();
+                DKPnumlabel.Text = DKPnumbercaxun;
+            }
+            
+        }
+
+        private void read_Click(object sender, EventArgs e)//读取到列表1
+        {
+
+            checkedListBox1.Items.Clear();
+            string constr = string.Format("Database=Exam;Data Source = 69.194.11.204; User Id = root; Password=Abc123.@;CharSet=utf8;port=3306;Allow User Variables=True");
+            MySqlConnection conn = new MySqlConnection(constr);
+            string sqlcom = string.Format("select Name from DKP");
+            MySqlDataAdapter sda = new MySqlDataAdapter(sqlcom, conn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            //checkedListBox1 = dt;
+            //checkedListBox1.DisplayMember = "Name";
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                this.checkedListBox1.Items.Add(dt.Rows[i][0], false);//设置不选定                            
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "增加")
+            {
+                int dkpaddnum = Convert.ToInt32(addDKPnum.Text);
+                string constr = string.Format("Database=Exam;Data Source = 69.194.11.204; User Id = root; Password=Abc123.@;port=3306;Allow User Variables=True;CharSet=utf8;");
+                MySqlConnection conn = new MySqlConnection(constr);
+                conn.Open();
+                string selectedItem = string.Empty;
+                for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                {
+                    if (checkedListBox1.GetItemChecked(i))
+                    {
+                        selectedItem = checkedListBox1.Items[i].ToString();
+                        string sqlcom = string.Format("update DKP Set DKPTotal = DKPTotal+{0} where Name='{1}';", dkpaddnum, selectedItem);
+                        MySqlCommand sqlcommd = new MySqlCommand(sqlcom, conn);
+                        sqlcommd.ExecuteNonQuery();
+                    }
+                }
+                for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                {
+                    if (checkedListBox1.GetItemChecked(i))
+                    {
+                        selectedItem = selectedItem + "   " + checkedListBox1.Items[i].ToString();
+
+
+                    }
+
+
+                }
+
+                string remark = textRemark.Text;
+                string time = DateTime.Now.ToString();
+                string sqlcomlog = string.Format("insert into modifyLog values ('','{0}','{1}','加{2}','{3}')", remark, selectedItem,dkpaddnum,time);
+                MySqlCommand sqlcommdlog = new MySqlCommand(sqlcomlog, conn);
+                sqlcommdlog.ExecuteNonQuery();
+                MessageBox.Show("增加DKP成功");
+
+                conn.Close();
+
+            }
+
+
+
+
+            else if (comboBox1.Text == "减少")
+            {
+                int dkpaddnum = Convert.ToInt32(addDKPnum.Text);
+                string constr = string.Format("Database=Exam;Data Source = 69.194.11.204; User Id = root; Password=Abc123.@;CharSet=utf8;port=3306;Allow User Variables=True");
+                MySqlConnection conn = new MySqlConnection(constr);
+                conn.Open();
+                string selectedItem = string.Empty;
+                for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                {
+                    if (checkedListBox1.GetItemChecked(i))
+                    {
+                        selectedItem = checkedListBox1.Items[i].ToString();
+                        string sqlcom = string.Format("update DKP Set DKPTotal = DKPTotal-{0} where Name = '{1}'", dkpaddnum, selectedItem);
+                        MySqlCommand sqlcommd = new MySqlCommand(sqlcom, conn);
+                        sqlcommd.ExecuteNonQuery();
+                    }
+                }
+                for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                {
+                    if (checkedListBox1.GetItemChecked(i))
+                    {
+                        selectedItem = selectedItem + "   " + checkedListBox1.Items[i].ToString();
+
+
+                    }
+                    
+                  
+                }
+                
+                string remark = textRemark.Text;
+                string time = DateTime.Now.ToString();
+                string sqlcomlog = string.Format("insert into modifyLog values ('','{0}','{1}','减{2}','{3}')", remark, selectedItem, dkpaddnum,time);
+                MySqlCommand sqlcommdlog = new MySqlCommand(sqlcomlog, conn);
+                sqlcommdlog.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("减少DKP成功");
+            }
+            else
+            {
+                MessageBox.Show("请选择添加或减少");
+            }
+
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Text = "增加";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
